ad7f8fcdaab243010fbe25bdc61418182203f461
shuhei0916 <shuheiito.stc@gmail.com>
2024-11-25 12:02:40 +0900

    Add a script in Player

---
+++ player.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+@export var speed = 400
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
